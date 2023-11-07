d1179c49c419b7734081b74cba6e3057bf59f2ed
fraolb <Fraolbereket@gmail.com>
2023-11-07 12:09:57 +0300

    test2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with fraol
+          Learn React with fraol again
         </a>
       </header>
     </div>
